0a87bbc1f57554efe06d92606efaf9455f8a09d7
nathi17lf <130677886+nathi17lf@users.noreply.github.com>
2024-05-03 12:23:01 -0300

    Add files via upload

--- projeto-de-vida--main/projeto-de-vida--main/NATHI/main.js
+++ projeto-de-vida--main/projeto-de-vida--main/NATHI/main.js
@@ -1,4 +1,5 @@
 const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll(".aba-conteudo");
 
 for(let i=0;i <botoes.length;i++){
     botoes[i] .onclick = function(){
@@ -8,6 +9,11 @@ for(let i=0;i <botoes.length;i++){
         }
 
         botoes[i].classList.add("ativo");
+        textos[i].classList.add("ativo");
     }
     
 }
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date ("2024-10-05T00:00:00");
+
+contadores[0].textContent = tempoObjetivo1
